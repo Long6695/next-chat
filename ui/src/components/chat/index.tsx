eba2c556247958b6baa8161b9c4fa2340595653f
Long6695 <thaihothanhlong95@gmail.com>
2023-04-25 15:34:10 +0700

    feat:integrate api rooms and conversations

--- ui/src/components/chat/index.tsx
+++ ui/src/components/chat/index.tsx
@@ -1,39 +1,45 @@
 import React from "react";
 import Message from "./Message";
 import Input from "./Input";
+import { MessageType, RoomsType } from "@/types/chat";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../loading";
 
-const Chat = () => {
+const fetchMessage = async (room_id: string) => {
+  if (!room_id) return;
+
+  const res = await fetch(`http://localhost:8080/conversations/${room_id}`);
+  const rooms = await res.json();
+  return rooms;
+};
+
+const Chat = ({ room }: { room: RoomsType }) => {
+  const {
+    data: message,
+    isLoading: isMessageLoading,
+    isFetching: isMessageFetching,
+  } = useQuery({
+    queryKey: ["hydrate-room", room?.room?.id],
+    queryFn: () => fetchMessage(room?.room?.id),
+  });
+
   return (
     <div className="h-full w-full bg-base-100 p-2 rounded-xl flex flex-col justify-end">
-      <Message
-        direction="chat-start"
-        name="Long"
-        status="Delivered"
-        message="Hello"
-        time="10:00"
-      />
-      <Message
-        direction="chat-end"
-        name="Long"
-        status="Delivered"
-        message="123123"
-        time="10:00"
-      />
-      <Message
-        direction="chat-start"
-        name="Long"
-        status="Delivered"
-        message="asdaskd"
-        time="10:00"
-      />
-      <Message
-        direction="chat-end"
-        name="Bot"
-        status="Delivered"
-        message="Hello"
-        time="10:00"
-      />
-      <Input/>
+      {isMessageLoading || isMessageFetching ? (
+        <Loading />
+      ) : message ? (
+        message?.map((item: MessageType) => (
+          <Message
+            key={item?.id}
+            direction={item?.user_id === '1bc833808-05ed-455a-9d26-64fe1d96d62d' ? "chat-end" : "chat-start"}
+            name={room?.users?.find(val => val?.id === item?.user_id)?.username || ""}
+            status="Delivered"
+            message={item?.content}
+            time={item?.created_at}
+          />
+        ))
+      ) : null}
+      <Input />
     </div>
   );
 };
---
+++ ui/src/components/rooms/Room.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Avatar from "../avatar";
+import { RoomType, RoomsType, UserType } from "@/types/chat";
+
+const Room = ({
+  data,
+  onSelected,
+}: {
+  data: { users: UserType[]; room: RoomType };
+  onSelected: (room: RoomsType) => void;
+}) => {
+  const date = new Date(data?.room?.created_at);
+  const ampm = date.getHours() >= 12 ? "PM" : "AM";
+  const time = `${date.getHours()}:${date.getMinutes()} ${ampm}`;
+  const specificDate = `${date.getDate()}/${date.getMonth()}`;
+
+  return (
+    <div className="flex items-center mt-2" onClick={() => onSelected(data)}>
+      <Avatar name={data?.room?.name} className="w-12 h-12" />
+      <div className="flex-1">
+        <h2 className="text-md text-base-content text-bold">
+          {data?.room?.name}
+        </h2>
+        <p className="text-sm text-base-content">{data?.room?.last_message}</p>
+      </div>
+      <div>
+        <p className="text-xs text-base-content">{specificDate}</p>
+        <p className="text-xs text-base-content">{time}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Room;
